979d0852dac44e59f41c763a111bff11d0b49c6c
MayconInforgames <maycon_gus2009@hotmail.com>
2021-01-05 18:00:35 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -15,11 +15,11 @@ import com.devsuperior.dsdeliver.repositories.ProductRepository;
 public class ProductService {
 
 	@Autowired
-	private ProductRepository productRepository;
+	private ProductRepository repository;
 
 	@Transactional(readOnly = true)
 	public List<ProductDTO> findAll() {
-		List<Product> list = productRepository.findAllByOrderByNameAsc();
+		List<Product> list = repository.findAllByOrderByNameAsc();
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
 
